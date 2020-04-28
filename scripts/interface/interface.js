bc5d374824a70f25b530d3419ccb6c86e02e9ffe
Archalias <naturezprofit@gmail.com>
2020-04-27 22:57:48 -0700

    refactoring / cleanup

--- scripts/interface/interface.js
+++ scripts/interface/interface.js
@@ -4,13 +4,26 @@ import analyticsButton from './analyticsTemplate.html';
 import inventorySortButton from './inventorySortButtonTemplate.html';
 import statsHtml from './statsTemplate.html';
 
-((parent) => {
-  const { $, socket, entities } = parent;
+(() => {
+  const { $, socket } = parent;
   let state = {
     sorting: false,
   };
   let combatLog = [];
   let lootLog = [];
+  let showAnalytics = true;
+
+  // Add interface elements
+  $('#newparty').before(statsHtml);
+  $('#toprightcorner').find('span.codebuttons').after(analyticsButton);
+  // Setup interface events
+  $('#arc_b_analytics').click(toggleAnalytics);
+
+  // State based interface initializations
+  if (!showAnalytics) $('[id^=arc_meters]').hide();
+  if ($('#bottomleftcorner').find('[class$=theinventory]').length) {
+    if (!$('#arc_b_isort').length) addInventorySortButton();
+  }
 
   // Register listener to cleanup when script terminates
   window.addEventListener('unload', cleanup);
@@ -36,7 +49,7 @@ import statsHtml from './statsTemplate.html';
     'hit',
     (data) => combatLog.push({
       ...data,
-      xp: data.kill && (entities[data.id]?.xp || 0),
+      xp: data.kill && (parent.entities[data.id]?.xp || 0),
       time: Date.now()
     }),
   ];
@@ -49,63 +62,62 @@ import statsHtml from './statsTemplate.html';
   ];
   socket.on(...parent.chestLootSocket);
 
-  // Add interface elements
-  $('#newparty').before(statsHtml);
-  $('#toprightcorner').find('span.codebuttons').after(analyticsButton);
-  // Setup interface events
-  $('#analyticsbutton').click(toggleAnalytics);
-
-  // State based interface initializations
-  if (!parent.showAnalytics) $('[id^=analyticsmeters]').hide();
-  if ($('#bottomleftcorner').find('[class$=theinventory]').length) {
-    if (!$('#inventorysortb').length) addInventorySortButton();
-  }
 
   // Interval for log parsing/meters
-  parent.dpsInterval = setInterval(() => {
-    if (!parent.showAnalytics) return;
-    const currentTime = Date.now();
-    const targetTime = currentTime - 300000;
+  let dpsInterval = setInterval(calculateStats, 250);
+
+  function calculateStats() {
+    if (!showAnalytics) return;
+    const targetTime = Date.now() - 300000;
 
     combatLog = combatLog.filter(({ time }) => time > targetTime);
     lootLog = lootLog.filter(({ time }) => time > targetTime);
 
-    parent.dps = 0;
+    let totals = {
+      xp: 0,
+      dps: 0,
+      first: Date.now(),
+    };
 
-    const players = parent.party_list.length ? [...parent.party_list] : [parent.character.name];
+    const players = parent.party_list.length ? [...parent.party_list] : [character.name];
     players.forEach(player => {
-      const log = combatLog.filter(({ hid }) => hid === player);
-      // Calculate dps for last 60 seconds
-      let dps = log.reduce((totalDamage, { damage }) => totalDamage + (damage || 0), 0);
-      let hps = log.reduce((totalHeal, { heal, lifesteal }) => totalHeal + (heal || 0) + (lifesteal || 0), 0);
+      // Calculate metrics for last 60 seconds, why parse this every single loop....
+      let metrics = combatLog.reduce((total, { hid, time, xp, damage, heal, lifesteal }) => {
+        if (hid === player) {
+          total.dps += (damage || 0);
+          total.hps += (heal || 0) + (lifesteal || 0);
+          total.xp += (xp || 0);
+          if (time < total.first) total.first = time;
+        }
+        return total;
+      }, { dps: 0, hps: 0, xp: 0, first: Date.now() });
+      if (metrics.first < totals.first) totals.first = metrics.first;
+      metrics.first = (Date.now() - metrics.first) / 1000;
       // Limit dps meter to oldest entry
-      const firstLogTime = currentTime - log.reduce((oldestTime, { time }) => time < oldestTime ? time : oldestTime, currentTime);
-      dps = Math.round(dps / (firstLogTime / 1000));
-      hps = Math.round(hps / (firstLogTime / 1000));
-      parent.dps += dps;
-      $(`#dps_${player}`).text(`${to_pretty_num(dps || 0)}`);
-      $(`#hps_${player}`).text(`${to_pretty_num(hps || 0)}`);
+      metrics.dps = Math.round(metrics.dps / metrics.first);
+      metrics.hps = Math.round(metrics.hps / metrics.first);
+      totals.dps += metrics.dps || 0;
+      totals.xp += metrics.xp || 0;
+      $(`#dps_${player}`).text(`${metrics.dps || 0}`.replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $(`#hps_${player}`).text(`${metrics.hps || 0}`.replace(/\B(?=(\d{3})+(?!\d))/g, ","));
     });
-    const log = combatLog.filter(({ hid }) => parent.party_list.some(player => player === hid));
-    // Limit meters to oldest entry
-    const firstLogTime = currentTime - log.reduce((oldestTime, { time }) => time < oldestTime ? time : oldestTime, currentTime);
-    let xph = log.reduce((totalXp, { xp }) => totalXp + (xp ? xp : 0), 0);
-    xph = Math.round(xph * (1 / (firstLogTime / 3600000)));
+    totals.first = (Date.now() - totals.first) / 1000;
+
+    let xph = Math.round(totals.xp * (1 / (totals.first / 3600)));
     let gph = lootLog.reduce((totalGold, { gold }) => totalGold + (gold || 0), 0);
-    gph = Math.round(gph * (1 / (firstLogTime / 3600000)));
+    gph = Math.round(gph * (1 / (totals.first / 3600)));
 
-    $(`#stats_xph`).text(`${to_pretty_num(xph || 0)}`);
-    $(`#stats_dps`).text(`${parent.to_pretty_num(parent.dps || 0)}`);
-    $(`#stats_gph`).text(`${to_pretty_num(gph || 0)}`);
-  }, 250);
-
+    $(`#stats_xph`).text(`${xph || 0}`.replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    $(`#stats_dps`).text(`${totals.dps || 0}`.replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    $(`#stats_gph`).text(`${gph || 0}`.replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+  }
   // Functions
   function toggleAnalytics() {
-    parent.showAnalytics = !parent.showAnalytics;
-    if (parent.showAnalytics) {
-      $('[id^=analyticsmeters]').show();
+    showAnalytics = !showAnalytics;
+    if (showAnalytics) {
+      $('[id^=arc_meters]').show();
     } else {
-      $('[id^=analyticsmeters]').hide();
+      $('[id^=arc_meters]').hide();
     }
   }
 
@@ -117,7 +129,7 @@ import statsHtml from './statsTemplate.html';
         if (!playerDiv.find(`[id^=partyframe_${player}]`).length) {
           playerDiv.wrap(`<div id="partyframe_${player}" style="display: inline-block; margin-right: 4px;"></div>`);
           playerDiv.after(`
-            <div id="analyticsmeters" style="position: absolute; margin: -24px 0px 0px 0px">
+            <div id="arc_meters" style="position: absolute; margin: -24px 0px 0px 0px">
               <div id="dps_${player}" style="background: black; border: 4px solid gray; width: 52px; margin-bottom: 2px;padding: 2px; display: block; text-align: left;">0</div>
               <div id="hps_${player}" style="color: #FF2E46; background: black; border: 4px solid gray; width: 52px; margin-bottom: 2px; margin-top: -6px; padding: 2px; display: block; text-align: left;">0</div>
             </div>
@@ -125,57 +137,59 @@ import statsHtml from './statsTemplate.html';
         }
       }
     });
-    if (!parent.showAnalytics) $('[id^=analyticsmeters]').hide();
+    calculateStats();
+    if (!showAnalytics) $('[id^=arc_meters]').hide();
   }
 
   function addInventorySortButton() {
     // Inventory sort button
     $('[class$=theinventory]').find('[class=goldnum]').parent().before(inventorySortButton);
-    $('#inventorysortb').hover(
-      () => { $('#inventorysortb').css({ 'background-color': 'gray', 'border-color': 'lightgray' }); },
-      () => { $('#inventorysortb').css({ 'background-color': '', 'border-color': '' }); }
+    $('#arc_b_isort').hover(
+      () => { $('#arc_b_isort').css({ 'background-color': 'gray', 'border-color': 'lightgray' }); },
+      () => { $('#arc_b_isort').css({ 'background-color': '', 'border-color': '' }); }
     );
-    $('#inventorysortb').click(() => {
+    $('#arc_b_isort').click(() => {
       if (state.sorting) return -1;
       state.sorting = true;
       let totalSwaps = sortInventory();
       let animateSortIcon = setInterval(() => {
-        $('#inventorysortb').text(['-', '\\', '|', '/'][Math.floor((Date.now() % 500) / 125)]);
+        $('#arc_b_isort').text(['-', '\\', '|', '/'][Math.floor((Date.now() % 500) / 125)]);
       }, 25);
       setTimeout(() => {
         state.sorting = false;
         clearInterval(animateSortIcon);
-        $('#inventorysortb').text('⇅');
+        $('#arc_b_isort').text('⇅');
       }, 25 * totalSwaps + 500);
     });
-    $('#inventorysortb').mousedown((event) => event.which === 3 && (() => {
+    $('#arc_b_isort').mousedown((event) => event.which === 3 && (() => {
       if (state.sorting) return -1;
       state.sorting = true;
       let totalSwaps = scrambleInventory();
       let animateSortIcon = setInterval(() => {
-        $('#inventorysortb').text(['-', '\\', '|', '/'][Math.floor((Date.now() % 500) / 125)]);
+        $('#arc_b_isort').text(['-', '\\', '|', '/'][Math.floor((Date.now() % 500) / 125)]);
       }, 25);
       setTimeout(() => {
         state.sorting = false;
         clearInterval(animateSortIcon);
-        $('#inventorysortb').text('⇅');
+        $('#arc_b_isort').text('⇅');
       }, 25 * totalSwaps + 500);
     })());
   }
 
   function cleanup() {
     // Clean up interface
-    $('#analyticsbutton').remove();
+    $('#arc_b_analytics').remove();
     $('#chancefulldiv').remove();
     $('[id^=partyframe_]').remove();
-    $('[id^=analyticsmeters]').remove();
-    $('#inventorysortb').remove();
+    $('[id^=arc_meters]').remove();
+    $('#arc_b_isort').remove();
     // Deregister sockets
     if (parent.combatLogSocket) socket.off(...parent.combatLogSocket);
     if (parent.chestLootSocket) socket.off(...parent.chestLootSocket);
     // Clear intervals
-    if (parent.dpsInterval) clearInterval(parent.dpsInterval);
+    if (dpsInterval) clearInterval(dpsInterval);
     // Restore base functions
     if (parent.renderInventory) parent.render_inventory = parent.renderInventory;
+    if (parent.renderParty) parent.render_party = parent.renderParty;
   }
-})(parent);
+})();
